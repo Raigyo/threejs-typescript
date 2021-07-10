c5e758f8509e6ea6f87906f29e3f4e6db1e10c20
chilot <https://github.com/Raigyo>
2021-07-10 17:17:20 +0200

    menu burger responsive ok

--- Three.js-TypeScript-Tutorial/dist/client/partials/header.js
+++ Three.js-TypeScript-Tutorial/dist/client/partials/header.js
@@ -1,31 +1,46 @@
 const headerTemplate = document.createElement("template");
 headerTemplate.innerHTML = `
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <style>
-body {
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-.navbar {
-  overflow: hidden;
+/* Add a black background color to the top navigation */
+.topnav {
   background-color: #333;
+  overflow: hidden;
+  
 }
 
-.navbar a {
+/* Style the links inside the navigation bar */
+.topnav a {
   float: left;
-  font-size: 16px;
-  color: white;
+  display: block;
+  color: #f2f2f2;
   text-align: center;
   padding: 14px 16px;
   text-decoration: none;
+  font-size: 17px;
 }
 
+/* Add an active class to highlight the current page */
+.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
+/* Hide the link that should open and close the topnav on small screens */
+.topnav .icon {
+  display: none;
+}
+
+/* Dropdown container - needed to position the dropdown content */
 .dropdown {
   float: left;
   overflow: hidden;
 }
 
+/* Style the dropdown button to fit inside the topnav */
 .dropdown .dropbtn {
-  font-size: 16px;  
+  font-size: 17px;
   border: none;
   outline: none;
   color: white;
@@ -35,10 +50,7 @@ body {
   margin: 0;
 }
 
-.navbar a:hover, .dropdown:hover .dropbtn {
-  background-color: green;
-}
-
+/* Style the dropdown content (hidden by default) */
 .dropdown-content {
   display: none;
   position: absolute;
@@ -48,6 +60,7 @@ body {
   z-index: 1;
 }
 
+/* Style the links inside the dropdown */
 .dropdown-content a {
   float: none;
   color: black;
@@ -57,19 +70,68 @@ body {
   text-align: left;
 }
 
-.dropdown-content a:hover {
-  background-color: #2FA1D6;
+/* Add a dark background on topnav links and the dropdown button on hover */
+.topnav a:hover, .dropdown:hover .dropbtn {
+  background-color: #555;
+  color: white;
 }
 
+/* Add a grey background to dropdown links on hover */
+.dropdown-content a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Show the dropdown menu when the user moves the mouse over the dropdown button */
 .dropdown:hover .dropdown-content {
   display: block;
 }
+
+/* When the screen is less than 600 pixels wide, hide all links, except for the first one ("Home"). Show the link that contains should open and close the topnav (.icon) */
+@media screen and (max-width: 600px) {
+  .topnav a:not(:first-child), .dropdown .dropbtn {
+    display: none;
+  }
+  .topnav a.icon {
+    float: right;
+    display: block;
+  }
+}
+
+/* The "responsive" class is added to the topnav with JavaScript when the user clicks on the icon. This class makes the topnav look good on small screens (display the links vertically instead of horizontally) */
+@media screen and (max-width: 600px) {
+  .topnav {z-index: 1000}
+  .topnav.responsive {
+    position: relative;
+    height: 500px;
+    overflow: hidden;
+    overflow-y: scroll;
+  }
+  .topnav.responsive a.icon {
+    position: absolute;
+    right: 0;
+    top: 0;
+  }
+  .topnav.responsive a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+  .topnav.responsive .dropdown {float: none;}
+  .topnav.responsive .dropdown-content {position: relative; display: block;}
+  .topnav.responsive .dropdown .dropbtn {
+    display: block;
+    width: 100%;
+    text-align: left;
+  }
+}
 </style>
-<div class="navbar">
-  <a href="#home">Home</a>
+
+<div class="topnav" id="myTopnav">
+  <a href="/" class="active">Home</a>
   <a href="../components/geometry/geometry.html">Geometry</a>
   <div class="dropdown">
-    <button class="dropbtn">Materials 
+    <button class="dropbtn">Materials
       <i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-content">
@@ -107,8 +169,11 @@ body {
       <a href="#">Link 2</a>
       <a href="#">Link 3</a>
     </div>
-  </div> 
+  </div>
+  <a href="#about">About</a>
+  <a href="javascript:void(0);" class="icon">&#9776;</a>
 </div>
+
 `;
 
 class Header extends HTMLElement {
@@ -119,7 +184,20 @@ class Header extends HTMLElement {
 
   connectedCallback() {
     const shadowRoot = this.attachShadow({ mode: "open" });
+
     shadowRoot.appendChild(headerTemplate.content);
+    const burgerBtn = shadowRoot.querySelector("#myTopnav");
+    // console.log("widget: ", widget);
+    burgerBtn.addEventListener("click", (event) => {
+      console.log("clicked");
+      const x = shadowRoot.querySelector("#myTopnav");
+      console.log(x);
+      if (x.className === "topnav") {
+        x.className += " responsive";
+      } else {
+        x.className = "topnav";
+      }
+    });
   }
 }
 
